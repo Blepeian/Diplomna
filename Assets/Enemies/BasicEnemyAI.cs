02b7c299c8e66adf2ed8d8990a8b03b2709998c1
Kalin Asenov <kalin.k.asenov@gmail.com>
2022-02-18 12:11:48 +0200

    enemies follow player when in aggro range

--- Assets/Enemies/BasicEnemyAI.cs
+++ Assets/Enemies/BasicEnemyAI.cs
@@ -2,34 +2,100 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum enemyState{
+    Idle, 
+    Attacking
+}
+
 public class BasicEnemyAI : MonoBehaviour
 {
     public float moveSpeed;
+    public enemyState state;
     public float moveDistance;
-    private bool hasStartedMoving = false;
-    [SerializeField] private float startX;
+    public GameObject player;
+    public float attackRange;
+    public Rigidbody2D body;
+    public float maxFollowDistance;
+    public bool returning = false;
+
+    private float sightDistance;
+    private bool lookingRight = true;
+    private float velocity;
+    [SerializeField] private Vector2 start;
+    private LayerMask playerLayer;
     
     void Start()
     {
-        startX = transform.position.x;
+        start = transform.position;
+        state = enemyState.Idle;
+        player = GameObject.Find("Player");
+        body = gameObject.GetComponent<Rigidbody2D>();
+        playerLayer = LayerMask.GetMask("Player");
+        velocity = -moveSpeed;
+        sightDistance = attackRange * 2;
     }
 
     void Update()
     {
-        if(!hasStartedMoving)
+        if(Vector2.Distance(transform.position, player.transform.position) <= sightDistance)
         {
-            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+            state = enemyState.Attacking;
         }
 
-        if((transform.position.x - startX) <= -moveDistance)
+        if(state == enemyState.Idle)
         {
-            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
-            transform.Rotate(0f, 180f, 0f);
+            if(Vector2.Distance(start, transform.position) >= moveDistance)
+            {
+                if(!lookingRight)
+                {
+                    velocity = moveSpeed;
+                }
+                else if(lookingRight)
+                {
+                    velocity =  -moveSpeed;
+                }
+
+                transform.Rotate(0f, 180f, 0f);
+                lookingRight = !lookingRight;
+            }
+            body.velocity = new Vector2(velocity, body.velocity.y);
         }
-        if((transform.position.x - startX) >= moveDistance)
+        
+        if(state == enemyState.Attacking)
         {
-            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-            transform.Rotate(0f, 180f, 0f);
+            if(Vector2.Distance(start, transform.position) <= maxFollowDistance)
+            {
+                if(Vector2.Distance(player.transform.position, transform.position) <= attackRange)
+                {
+                    velocity = 0;
+                }
+                else if(player.transform.position.x > transform.position.x)
+                {
+                    velocity = moveSpeed;
+                    lookingRight = true;
+                }
+                else if(player.transform.position.x < transform.position.x)
+                {
+                    velocity =  -moveSpeed;
+                    lookingRight = false;
+                }
+            }
+            else if(Vector2.Distance(start, transform.position) > maxFollowDistance)
+            {
+                transform.position = start;
+                state = enemyState.Idle;
+            }
+
+            if(velocity > 0)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            else if(velocity < 0)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+
+            body.velocity = new Vector2(velocity, body.velocity.y);
         }
     }
 }
