2141d6546491583d34e6b5e08405923a821168ba
Kalin Asenov <kalin.k.asenov@gmail.com>
2022-03-09 20:59:40 +0200

    made axe swing from side to side

--- Assets/Environment/SwingingAxe.cs
+++ Assets/Environment/SwingingAxe.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class SwingingAxe : MonoBehaviour
 {
     public float damage;
-
     public float rotSpeed;
+    public float maxSwing;
     public Transform rotPoint;
+    
      
     private Vector3 zAxis = new Vector3(0, 0, 1);
+    private float startX;
+
+    void Awake()
+    {
+        startX = gameObject.transform.position.x;
+    }
  
     void FixedUpdate () {
+        if(System.Math.Abs(startX - gameObject.transform.position.x) >= maxSwing)
+        {
+            rotSpeed = -1 * rotSpeed;
+        }
         transform.RotateAround(rotPoint.position, zAxis, rotSpeed);
     }
 
