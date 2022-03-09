2c41f4b872697362eb55cdaf124da9e5688ccc52
Kalin Asenov <kalin.k.asenov@gmail.com>
2022-03-09 10:55:04 +0200

    made axe spin, will make swing later

--- Assets/Environment/SwingingAxe.cs
+++ Assets/Environment/SwingingAxe.cs
@@ -6,6 +6,15 @@ public class SwingingAxe : MonoBehaviour
 {
     public float damage;
 
+    public float rotSpeed;
+    public Transform rotPoint;
+     
+    private Vector3 zAxis = new Vector3(0, 0, 1);
+ 
+    void FixedUpdate () {
+        transform.RotateAround(rotPoint.position, zAxis, rotSpeed);
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(col.gameObject.CompareTag("Player"))
