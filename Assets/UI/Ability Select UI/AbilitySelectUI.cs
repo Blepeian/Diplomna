3715a4dcc14c751eb99e5c66ca6df64dd26e686a
Kalin Asenov <kalin.k.asenov@gmail.com>
2022-03-11 12:39:36 +0200

    finished ui for ability exchange

--- Assets/UI/Ability Select UI/AbilitySelectUI.cs
+++ Assets/UI/Ability Select UI/AbilitySelectUI.cs
@@ -10,6 +10,7 @@ public class AbilitySelectUI : MonoBehaviour
     public Text abDescription;
 
     private Ability ability;
+    private AbilityItem currItem;
 
     private void Awake()
     {
@@ -17,8 +18,32 @@ public class AbilitySelectUI : MonoBehaviour
         gameObject.SetActive(false);
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown("1"))
+        {
+            SetAbility(1);
+        }
+
+        if(Input.GetKeyDown("2"))
+        {
+            SetAbility(2);
+        }
+
+        if(Input.GetKeyDown("3"))
+        {
+            SetAbility(3);
+        }
+
+        if(Input.GetButtonDown("Cancel"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
     public void GetAbility(AbilityItem abItem)
     {
+        currItem = abItem;
         ability = abItem.ability;
         abIcon.sprite = abItem.gameObject.GetComponent<SpriteRenderer>().sprite;
         abName.text = abItem.itemName;
@@ -29,5 +54,7 @@ public class AbilitySelectUI : MonoBehaviour
     public void SetAbility(int slot)
     {
         GameObject.Find("Player").GetComponentInChildren<PlayerAbilities>().SetAbility(slot, ability);
+        Destroy(currItem.gameObject);
+        gameObject.SetActive(false);
     }
 }
